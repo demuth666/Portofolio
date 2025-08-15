af89e63fdb9ae476e8441797a23b093163e6ae95
Ahmad Taupik Paisal <97859391+demuth666@users.noreply.github.com>
2025-08-15 12:00:23 +0700

    Update HeroText.jsx

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -61,7 +61,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi,I'm Ali
+          Hi,I'm Ahmad
         </motion.p>
         <div>
           <motion.p
